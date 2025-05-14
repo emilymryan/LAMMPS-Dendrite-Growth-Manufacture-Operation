f793fb2354e39a254e3abde8e38b619defe5d5cb
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2025-05-14 10:58:57 -0400

    Add files via upload

--- runLAMMPS_Cycling/run.sh
+++ runLAMMPS_Cycling/run.sh
@@ -1,16 +1,15 @@
-#! /bin/bash
+ #! /bin/bash
 module load openmpi/3.1.1
 module load python3/3.6.5
 
 
-lmp=/projectnb/ryanlab/mmorey/mylammps/src/lmp_mpi
+lmp=/projectnb/ryanlab/mmorey/mylammps_29Aug2024/src/lmp_mpi
 
-
-dname=CMES_Case1_test
-output=/projectnb/ryanlab/mmorey/CMESPaper/tests/${dname}
+dname=test_16April25_1
+output=/projectnb/ryanlab/mmorey/SPHPaper/Cycling/tests/${dname}
 dname1=${output}
 mkdir -p ${dname1}
 
-${lmp} -in input.lmp -var dname ${dname1}
+${lmp} -in input_RPP_SE.lmp -var dname ${dname1}
 
 exit
---
+++ runLAMMPS_Cycling/submit.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -l
+#$ -l h_rt=90:00:00
+#$ -P ryanlab
+#$ -m bea
+#$ -j y
+#$ -N PP
+#$ -o /projectnb/ryanlab/mmorey/SPH_Paper/newresults/final/$JOB_NAME.o$JOB_ID 
+#$ -e /projectnb/ryanlab/mmorey/SPH_Paper/newresults/final/$JOB_NAME.e$JOB_ID 
+#$ -pe mpi_16_tasks_per_node 32
+module load openmpi/3.1.1
+module load python3/3.6.5
+
+lmp=/projectnb/ryanlab/mmorey/mylammps_29Aug2024/src/lmp_mpi
+
+dname1=CMES_16April_test_${JOB_ID}
+output=/projectnb/ryanlab/mmorey/SPH_Paper/newresults/${dname1}
+dname=${output}
+mkdir -p ${output}
+
+mpirun -np $NSLOTS ${lmp} -in input_RPP_SE.lmp -var dname ${dname}
+
+exit
+
