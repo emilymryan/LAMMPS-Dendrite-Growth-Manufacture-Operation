e78b69708c9fd8e112b8b5476b5b462d7948afb5
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2025-05-14 10:54:54 -0400

    Add files via upload

---
+++ runLAMMPS_Cycling/run.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+module load openmpi/3.1.1
+module load python3/3.6.5
+
+
+lmp=/projectnb/ryanlab/mmorey/mylammps/src/lmp_mpi
+
+
+dname=CMES_Case1_test
+output=/projectnb/ryanlab/mmorey/CMESPaper/tests/${dname}
+dname1=${output}
+mkdir -p ${dname1}
+
+${lmp} -in input.lmp -var dname ${dname1}
+
+exit
